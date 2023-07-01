9bc37623eab7e25cb9e2abef963586339313deb5
Aboood <AbdulmalekJayash@gmail.com>
2023-07-01 21:44:03 +0300

    delete unwant setting  screens from admin site

--- admin/setting/index.php
+++ admin/setting/index.php
@@ -1,167 +1,149 @@
-
 <?php
   session_start();
+
   include('../../includes/lib.php');
   include_once('../../includes/setting.php');
   checkAdminSession();
 
-  $pageTitle = lang("Settings");
+  $pageTitle = lang("Update Setting");
+  //$row = new Setting(null);
+   $id =  $return_days =  $fine_amount =  $student_max_issue = "";
+  //$id = $name = $manager = $managerPhone = $agent = $agentPhone = $kindergarten = $earlyChildhood = $elementary = $intermediate = $secondary = $active = "";
+  include('../../template/header.php'); 
+  $errors = array();
+
+
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') 
+  {
+      $_SESSION["message"] = '';
+      $result = GetSetting();
+
+      if( count( $result ) > 0)
+      {
+        $row = $result[0];
+        $id = $row['id'];
+        $return_days = $row['return_days'];
+        $fine_amount = $row['fine_amount'];
+        $student_max_issue = $row['student_max_issue'];
+      }
+  }
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+  {
+    if(isset($_POST['updateSetting']))
+    {
+        $id = $_POST['id'];
+        $return_days = $_POST['return_days'];
+        $fine_amount = $_POST['fine_amount'];
+        $student_max_issue = $_POST['student_max_issue'];
+      if( empty($return_days)){
+        $errors[] = "<li>" . lang("Return Days is requierd") . "</li>";
+        $_SESSION["fail"] .= "<li>" . lang("Return Days is requierd") . "</li>";
+        }
+      if( empty($fine_amount)){
+        $errors[] = "<li>" . lang("Fine Amount is requierd") . "</li>";
+        $_SESSION["fail"] .= "<li>" . lang("Fine Amount is requierd") . "</li>";
+        }
+      if( empty($student_max_issue)){
+        $errors[] = "<li>" . lang("Student Max Issues is requierd") . "</li>";
+        $_SESSION["fail"] .= "<li>" . lang("Student Max Issues is requierd") . "</li>";
+        }
+      
+      if(count($errors) == 0)
+      {
+
+        $result = GetSetting();
+        if( count( $result ) > 0)
+          $row = $result[0];
+        
+        $update = AddOrUpdateSetting( $return_days,  $fine_amount,  $student_max_issue, );
+        if($update ==  true)
+        {
+  
+          $_SESSION["message"] = lang("Setting Updated successfuly!");
+          $_SESSION["success"] = lang("Setting Updated successfuly!");
+          header('Location:'. $PATH_ADMIN_SETTING .'index.php');
+          exit();
+        }
+        else
+        {
+          $_SESSION["message"] = lang("Error when Update Data");
+          $_SESSION["fail"] = lang("Error when Update Data");
+          $errors[] = lang("Error when Update Data");
+        }
+        
+      }
+      else
+      {
+      }
+  
+    }
+  }
 ?>
 
-<?php include('../../template/header.php'); ?>
 <?php include('../../template/startNavbar.php'); ?>
 
 
 <main>
     <header class="page-header page-header-compact page-header-light border-bottom bg-white mb-4">
-        <div class="container-fluid px-4">
+        <div class="container-xl px-4">
             <div class="page-header-content">
                 <div class="row align-items-center justify-content-between pt-3">
                     <div class="col-auto mb-3">
                         <h1 class="page-header-title">
-                            <div class="page-header-icon"><i data-feather="users"></i></div>
-                            <?php echo lang("Setting List"); ?>
+                            <div class="page-header-icon"><i class="fa fa-school"></i></div>
+                            <?php echo lang("Update Setting"); ?>
                         </h1>
                     </div>
                     <div class="col-12 col-xl-auto mb-3">
-                        <a class="btn btn-sm btn-light text-primary" href="create.php">
-                            <i class="me-1" data-feather="plus"></i>
-                            <?php echo lang("Create New"); ?>
-                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </header>
     <!-- Main page content-->
-    <?php $all = getAllSettings(); ?>
-    <div class="container-fluid px-4">
-        <div class="card">
-            <div class="card-body">
-                <table id="datatablesSimple">
-                    <thead>
-                        <tr>
-                            <th><?php echo lang("ID"); ?></th>
-                            <th><?php echo lang("Return Days"); ?></th>
-                            <th><?php echo lang("Fine Amount"); ?></th>
-                            <th><?php echo lang("Student Max Issues"); ?></th>
-                            <th><?php echo lang("Actions"); ?></th>
-                        </tr>
-                    </thead>
-                    <!-- <tfoot>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Return Days</th>
-                                            <th>Fine Amount</th>
-                                            <th>Student Max Issues</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </tfoot> -->
-                    <tbody>
+    <div class="container-xl px-4 mt-4">
+        <div class="row">
+            <div class="col-xl-12">
+                <!-- Setting details card-->
+                <div class="card mb-4">
+                    <div class="card-header"><?php echo lang("Setting Data"); ?></div>
+                    <div class="card-body">
+                        <form action="" method="POST" enctype="multipart/form-data">
+                            <!-- Form Row-->
+                            <div class="row gx-3 mb-3">
+                                <input type="hidden" name="id" id="id" value="<?php echo $id;?>" />
+                                <!-- Form Group (return_days)-->
+                                <div class="col-md-4 mb-3">
+                                    <label class="small mb-1"
+                                        for="return_days"><?php echo lang("Return Days"); ?></label>
+                                    <input class="form-control" id="return_days" name="return_days" type="text"
+                                        placeholder="<?php echo lang("Return Days"); ?>"
+                                        value="<?php echo $return_days;?>" required />
+                                </div>
+                                <!-- Form Group (fine_amount)-->
+                                <div class="col-md-4 mb-3">
+                                    <label class="small mb-1"
+                                        for="fine_amount"><?php echo lang("Fine Amount"); ?></label>
+                                    <input class="form-control" id="fine_amount" name="fine_amount" type="text"
+                                        placeholder="<?php echo lang("Fine Amount"); ?>"
+                                        value="<?php echo $fine_amount;?>" required />
+                                </div>
+                                <!-- Form Group (student_max_issue)-->
+                                <div class="col-md-4 mb-3">
+                                    <label class="small mb-1"
+                                        for="student_max_issue"><?php echo lang("Student Max Issues"); ?></label>
+                                    <input class="form-control" id="student_max_issue" name="student_max_issue"
+                                        type="text" placeholder="<?php echo lang("Student Max Issues"); ?>"
+                                        value="<?php echo $student_max_issue;?>" required />
+                                </div>
 
-                        <!-- <tr> 
-                                            <td>Name</td>
-                                            <td>Mananger</td>
-                                            <td>Mananger Phone</td>
-                                            <td>Agent</td>
-                                            <td>Agent Phone</td>
-                                            <td>Active</td>
-                                            <td>
-                                                <button class="btn btn-datatable btn-icon btn-transparent-dark me-2"
-                                                    type="button" data-bs-toggle="modal"
-                                                    data-bs-target="#editSettingModal"><i
-                                                        data-feather="edit"></i></button>
-                                                <a class="btn btn-datatable btn-icon btn-transparent-dark" href="#!"><i
-                                                        data-feather="trash-2"></i></a>
-                                            </td>
-                                        </tr> -->
-                        <?php
-                                        foreach($all as $row)
-                                        {
-
-                                        ?>
-
-                        <tr>
-                                <td> <?php echo($row['id']); ?> </td>
-                                  <td> <?php echo($row['return_days']); ?> </td>
-                                  <td> <?php echo($row['fine_amount']); ?> </td>
-                                  <td> <?php echo($row['student_max_issue']); ?> </td>
-  
-                            <td>
-                                <a class="btn btn-datatable btn-icon btn-transparent-dark me-2"
-                                    href="edit.php?id=<?php echo($row['id']); ?>">
-                                    <i data-feather="edit"></i>
-                                </a>
-                                <a class="btn btn-datatable btn-icon btn-transparent-dark"
-                                    href="delete.php?id=<?php echo($row['id']); ?>">
-                                    <i data-feather="trash-2"></i>
-                                </a>
-                                <a class="btn btn-datatable btn-icon btn-transparent-dark"
-                                    href="detail.php?id=<?php echo($row['id']); ?>">
-                                    <i data-feather="eye"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        <?php }?>
-
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <!-- Create Setting modal-->
-    <div class="modal fade" id="createSettingModal" tabindex="-1" role="dialog" aria-labelledby="createSettingModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="createSettingModalLabel">Create New Setting</h5>
-                    <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="mb-0">
-                            <label class="mb-1 small text-muted" for="formSettingName">Setting
-                                Name</label>
-                            <input class="form-control" id="formSettingName" type="text"
-                                placeholder="Enter Setting name..." />
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-danger-soft text-danger" type="button"
-                        data-bs-dismiss="modal">Cancel</button>
-                    <button class="btn btn-primary-soft text-primary" type="button">Create New
-                        Setting</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Edit Setting modal-->
-    <div class="modal fade" id="editSettingModal" tabindex="-1" role="dialog" aria-labelledby="editSettingModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editSettingModalLabel">Edit Setting</h5>
-                    <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="mb-0">
-                            <label class="mb-1 small text-muted" for="formSettingName">Setting
-                                Name</label>
-                            <input class="form-control" id="formSettingName" type="text"
-                                placeholder="Enter Setting name..." value="Sales" />
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-danger-soft text-danger" type="button"
-                        data-bs-dismiss="modal">Cancel</button>
-                    <button class="btn btn-primary-soft text-primary" type="button">Save
-                        Changes</button>
+                            </div>
+                            <!-- Submit button-->
+                            <button name="updateSetting" class="btn btn-success"
+                                type="submit"><?php echo lang("Save"); ?></button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -169,8 +151,4 @@
 </main>
 
 
-
-
 <?php include('../../template/footer.php'); ?>
-
-
